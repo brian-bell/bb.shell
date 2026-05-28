cb007221a6689292edbbe796f35dd36862c6852f
Brian Bell <bellbm@gmail.com>
2026-05-28 19:50:20 -0400

    tweak prompt colors

--- prompt.zsh
+++ prompt.zsh
@@ -16,17 +16,27 @@ _bbzsh_git_dirty() {
   [[ -n $status_line ]]
 }
 
+_bbzsh_prompt_path() {
+  local path=$PWD
+  if [[ -n $HOME && $path == "$HOME" ]]; then
+    path='~'
+  elif [[ -n $HOME && $path == "$HOME"/* ]]; then
+    path="~/${path#$HOME/}"
+  fi
+  print -r -- "${path//\%/%%}"
+}
+
 _bbzsh_set_git_prompt() {
   local branch
   branch=$(_bbzsh_git_branch) || { _bbzsh_git_prompt=''; return; }
-  local segment="%F{blue}git:(%F{red}${branch}%F{blue})%f"
+  local segment="%F{yellow}git:%F{blue}(%F{red}${branch}%F{blue})%f"
   if _bbzsh_git_dirty; then
     segment+=" %F{yellow}✗%f"
   fi
-  _bbzsh_git_prompt="$segment"
+  _bbzsh_git_prompt=" $segment"
 }
 
 autoload -Uz add-zsh-hook
 add-zsh-hook precmd _bbzsh_set_git_prompt
 
-PROMPT='%(?:%F{green}➜:%F{red}➜)%f %F{cyan}%c%f ${_bbzsh_git_prompt} '
+PROMPT='%(?:%F{green}➜:%F{red}➜)%f %F{cyan}$(_bbzsh_prompt_path)%f${_bbzsh_git_prompt} %F{magenta}$%f '
